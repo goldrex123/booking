ab4aadc80112b9cce15ed86d32b0b70e756935bb
goldrex123 <goldrex123@naver.com>
2026-04-08 13:56:44 +0900

    feat: first commit project settings

---
+++ src/test/java/sky/ch/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.ch.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
